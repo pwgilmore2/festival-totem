6c2f1c6d5ff1c9a4cdbb176d9b9c35f519b988a4
Patrick Gilmore <91503642+pwgilmore2@users.noreply.github.com>
2026-09-21 22:22:36 -0500

    Load final text UI polish

--- secure_phone_server.py
+++ secure_phone_server.py
@@ -5,6 +5,7 @@ import sys
 import phone_server
 import controller_state_ui
 import chaos_layout_patch  # noqa: F401
+import text_ui_patch  # noqa: F401
 
 if Path(sys.argv[0]).name == "simulator.py":
     import simulator_display_patch  # noqa: F401
